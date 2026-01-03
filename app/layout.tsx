87e7259a20dc3e5567bffd1781e662d120f92e5f
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2026-01-03 09:41:34 +0545

    style(landing): responsiveness added

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" className="overflow-x-hidden">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
         >
           {children}
         </body>
