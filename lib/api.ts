d405a839691bfe672454c70ec972c05600fb78a6
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2026-01-12 17:45:15 +0545

    feat(api): automatic eror handling and json parsing with axios

--- lib/api.ts
+++ lib/api.ts
@@ -1,21 +1,17 @@
+import axios from "axios"
+
 const BASE_URL = "https://bee4d5d1-ade0-47fd-9697-f6bdbb23cb58.mock.pstmn.io"
 
+const api = axios.create({
+  baseURL: BASE_URL,
+})
+
 export async function getUsers() {
-  const response = await fetch(`${BASE_URL}/users`)
-  
-  if (!response.ok) {
-    throw new Error(`Failed to fetch users: ${response.statusText}`)
-  }
-  
-  return response.json()
+  const { data } = await api.get("/users")
+  return data
 }
 
 export async function getLands() {
-  const response = await fetch(`${BASE_URL}/lands`)
-  
-  if (!response.ok) {
-    throw new Error(`Failed to fetch lands: ${response.statusText}`)
-  }
-  
-  return response.json()
+  const { data } = await api.get("/lands")
+  return data
 }
